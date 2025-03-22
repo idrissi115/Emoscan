1451eef529ad2dc1b23b22b089ce15da03762333
idrissi115 <hamzaidrissi686@gmail.com>
2025-03-22 17:29:32 +0000

    Delete back directory

--- back/routes/presenceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getPresence, addPresence } = require("../controllers/presenceController");
-
-const router = express.Router();
-
-router.get("/", getPresence);
-router.post("/", addPresence);
-
-module.exports = router;
