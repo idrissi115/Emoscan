05a78fff7b966abe80b525554121df122116944a
idrissi115 <hamzaidrissi686@gmail.com>
2025-03-24 13:00:27 +0000

    Delete back directory

--- back/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
