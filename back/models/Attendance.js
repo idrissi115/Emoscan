b777b8ee9fa70895aa98ef8461e8d9cf7de8fe04
idrissi115 <hamzaidrissi686@gmail.com>
2025-03-24 11:44:18 +0000

    Delete back directory

--- back/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AttendanceSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  status: { type: String, enum: ["Present", "Absent"], required: true }
-});
-
-module.exports = mongoose.model("Attendance", AttendanceSchema);
