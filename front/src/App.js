45eb5a41357b0980f2381ecff9696c3d26b65532
mekouar1999 <othman.mekouar99@gmail.com>
2025-03-24 17:28:41 +0000

    rvhitecture without route

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Connexion from './component/connexion/Connexion';
+import Footer from './component/footer/Footer';
+import Inscription from './component/inscription/Inscription';
 import Navbar from './component/navbar/Navbar';
 
 function App() {
   return (
    <div>
 
-   <Navbar/>
+  <Navbar/>
+
+   <Inscription/>
+
    <Connexion/>
-    
+
+   <Footer/>
+
+   
+   
    </div>
   );
 }
--- front/src/component/footer/Footer.jsx
+++ front/src/component/footer/Footer.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-          <h1>Footer</h1>
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Section de gauche - Logo et Informations */}
+        <div className="footer-left">
+          <a href="/" className="footer-logo">
+            MyLogo
+          </a>
+          <p className="footer-description">
+            Votre source pour des solutions modernes et innovantes dans le domaine de la technologie.
+          </p>
+        </div>
 
-    </div>
-  )
-}
+        {/* Section centrale - Liens importants */}
+        <div className="footer-center">
+          <h3>Liens rapides</h3>
+          <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/about">À propos</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
 
-export default Footer
+        {/* Section de droite - Réseaux sociaux */}
+        <div className="footer-right">
+          <h3>Suivez-nous</h3>
+          <div className="social-links">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright Section */}
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} MyCompany. Tous droits réservés.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
